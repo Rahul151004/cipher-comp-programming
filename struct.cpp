99e72b0aa067a717870d8df83fc7521661a5de6e
Rahul <rbhat5480@gmail.com>
2025-01-20 14:56:27 +0530

    first commit

---
+++ struct.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Student {
+    string name;
+    int rollNumber;
+    float marks[3];
+};
+
+int main() {
+    Student* student = new Student;
+
+    cout << "Enter student's name: ";
+    getline(cin, student->name);
+    cout << "Enter roll number: ";
+    cin >> student->rollNumber;
+    cout << "Enter marks in 3 subjects: ";
+    for (int i = 0; i < 3; i++) {
+        cin >> student->marks[i];
+    }
+
+    float average = 0.0;
+    for (int i = 0; i < 3; i++) {
+        average += student->marks[i];
+    }
+    average /= 3;
+
+    cout << "\nStudent Details:" << endl;
+    cout << "Name: " << student->name << endl;
+    cout << "Roll Number: " << student->rollNumber << endl;
+    cout << "Marks: ";
+    for (int i = 0; i < 3; i++) {
+        cout << student->marks[i] << " ";
+    }
+    cout << endl;
+    cout << "Average Marks: " << average << endl;
+
+    delete student;
+
+    return 0;
+}
